f1632f921faf6a2b3d7b91257a4c24c34126978e
Luca Engelmann <lucaengelmann@posteo.de>
2024-09-18 19:15:21 +0200

    Basic functionality to retreive trace data

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -16,7 +16,13 @@ MainWindow::MainWindow(QWidget *parent)
     gpib->init(addr);
 
     instrument_gpib_id = 17;
+
+    lastAction = ACTION_NO_ACTION;
     request_instrument();
+
+    pollHold = new QTimer(this);
+    pollHold->setInterval(500);
+    QObject::connect(pollHold, &QTimer::timeout, this, &MainWindow::pollHold_timeout);
 
 }
 
@@ -41,54 +47,15 @@ void MainWindow::on_pushButton_clicked()
 void MainWindow::request_instrument()
 {
     // Is the 8751A on the bus?
-    lastCommand = "*IDN?";
-    gpib->send_command(instrument_gpib_id, lastCommand);
+    lastAction = ACTION_INSTRUMENT_REQUEST;
+    gpib->send_command(instrument_gpib_id, "*IDN?");
 }
 
 void MainWindow::init_instrument()
 {
-    currentIndex = 0;
-
     QVector<QString> commands;
     instrument_init_commands(commands);
-
-    QStateMachine *machine = new QStateMachine;
-    QState *s_sendCommand = new QState();
-    QState *s_response = new QState();
-    QFinalState *finish = new QFinalState();
-
-
-    s_response->addTransition(this, &MainWindow::positive_response_from_instrument, s_sendCommand);
-    s_sendCommand->addTransition(this, &MainWindow::command_sent, s_response);
-    s_sendCommand->addTransition(this, &MainWindow::instrument_init_finished, finish);
-
-    machine->addState(s_sendCommand);
-    machine->addState(s_response);
-    machine->addState(finish);
-    machine->setInitialState(s_sendCommand);
-
-    QObject::connect(s_sendCommand, &QState::entered, this, [=]{
-        qDebug() << "Send command. Index:" << currentIndex;
-        if (currentIndex < commands.size()) {
-            gpib->send_command(instrument_gpib_id, commands.at(currentIndex));
-            emit command_sent();
-        } else {
-            emit instrument_init_finished();
-        }
-    });
-
-    QObject::connect(s_response, &QState::entered, this, [&]{
-        qDebug() << "Response!";
-        currentIndex++;
-    });
-
-    QObject::connect(finish, &QState::entered, this, [=]{
-        qDebug() << "Init finished!";
-        ui->instrumentState->setText("Instrument initialized.");
-        machine->deleteLater();
-    });
-
-    machine->start();
+    send_command_list(commands);
 }
 
 void MainWindow::instrument_init_commands(QVector<QString> &commands)
@@ -115,14 +82,210 @@ void MainWindow::instrument_init_commands(QVector<QString> &commands)
 
 void MainWindow::gpib_response(QString resp)
 {
-    if (lastCommand == "*IDN?" && resp.contains("8751A")) {
-        qDebug() << "Instrument:" << resp;
-        lastCommand.clear();
-        init_instrument();
-        ui->instrumentState->setText("Instrument found.");
+    // Raw GPIB responses. Commands followed by the *OPC? command are handled seperately, See send_command_list_finished()
+    switch (lastAction) {
+    case ACTION_NO_ACTION:
+        break;
+    case ACTION_INSTRUMENT_REQUEST:
+        if (resp.contains("8751A")) {
+            init_instrument();
+            ui->instrumentState->setText("Instrument found.");
+            lastAction = ACTION_NO_ACTION;
+        }
+        break;
+    case ACTION_INSTRUMENT_INIT:
+    case ACTION_SETTINGS_UPDATE:
+    case ACTION_SWEEP_STARTED:
+    case ACTION_POLL_SWEEP_FINISH:
+    case ACTION_FIT_TRACE:
+        if (resp == "1\n") {
+            emit positive_response_from_instrument();
+        }
+        break;
+    case ACTION_TRANSFER_STIMULUS:
+        stimulus_raw.clear();
+        stimulus_raw = resp;
+        lastAction = ACTION_NO_ACTION;
+        get_trace_data();
+        break;
+    case ACTION_TRANSFER_DATA:
+        trace_raw.clear();
+        trace_raw = resp;
+        lastAction = ACTION_NO_ACTION;
+        unpack_raw_data();
+        break;
     }
-    if (resp == "1\n") {
-        emit positive_response_from_instrument();
+}
+
+void MainWindow::update_settings()
+{
+    instrumentSettings.clear();
+    instrumentSettings.push_back(QString("STAR %1;*OPC?").arg(ui->startFreq->text()));
+    instrumentSettings.push_back(QString("STOP %1;*OPC?").arg(ui->stopFreq->text()));
+    instrumentSettings.push_back(QString("POIN %1;*OPC?").arg(ui->numberOfPoints->currentText()));
+    instrumentSettings.push_back(QString("POWE %1;*OPC?").arg(ui->outputPower->value()));
+    if (ui->attenA->currentIndex() == 0) {
+        instrumentSettings.push_back("ATTIA0DB;*OPC?");
+    } else {
+        instrumentSettings.push_back("ATTIA20DB;*OPC?");
     }
+    if (ui->attenR->currentIndex() == 0) {
+        instrumentSettings.push_back("ATTIR0DB;*OPC?");
+    } else {
+        instrumentSettings.push_back("ATTIR20DB;*OPC?");
+    }
+    QString ifbw = ui->ifBw->currentText();
+    ifbw.chop(3); //Remove " Hz" unit
+    instrumentSettings.push_back(QString("IFBW %1;*OPC?").arg(ifbw));
+
+    send_command_list(instrumentSettings);
+}
+
+void MainWindow::send_command_list(QVector<QString> &commands)
+{
+    currentIndex = 0;
+
+    QStateMachine *machine = new QStateMachine;
+    QState *s_sendCommand = new QState();
+    QState *s_response = new QState();
+    QFinalState *finish = new QFinalState();
+
+
+    s_response->addTransition(this, &MainWindow::positive_response_from_instrument, s_sendCommand);
+    s_sendCommand->addTransition(this, &MainWindow::command_sent, s_response);
+    s_sendCommand->addTransition(this, &MainWindow::instrument_init_finished, finish);
+
+    machine->addState(s_sendCommand);
+    machine->addState(s_response);
+    machine->addState(finish);
+    machine->setInitialState(s_sendCommand);
+
+    QObject::connect(s_sendCommand, &QState::entered, this, [=]{
+        if (currentIndex < commands.size()) {
+            gpib->send_command(instrument_gpib_id, commands.at(currentIndex));
+            emit command_sent();
+        } else {
+            emit instrument_init_finished();
+        }
+    });
+
+    QObject::connect(s_response, &QState::entered, this, [&]{
+        currentIndex++;
+    });
+
+    QObject::connect(finish, &QState::entered, this, [=]{
+        send_command_list_finished();
+        machine->deleteLater();
+        s_sendCommand->deleteLater();
+        s_response->deleteLater();
+        finish->deleteLater();
+    });
+
+    machine->start();
+}
+
+void MainWindow::send_command_list_finished()
+{
+    switch (lastAction) {
+    case ACTION_NO_ACTION:
+        break;
+    case ACTION_INSTRUMENT_REQUEST:
+        break;
+    case ACTION_INSTRUMENT_INIT:
+        ui->instrumentState->setText("Instrument initialized.");
+        lastAction = ACTION_NO_ACTION;
+        break;
+    case ACTION_SETTINGS_UPDATE:
+        start_sweep();
+        lastAction = ACTION_NO_ACTION;
+        break;
+    case ACTION_SWEEP_STARTED:
+        pollHold->start();
+        lastAction = ACTION_NO_ACTION;
+        break;
+    case ACTION_POLL_SWEEP_FINISH:
+        pollHold->stop();
+        lastAction = ACTION_NO_ACTION;
+        fit_trace();
+        break;
+    case ACTION_FIT_TRACE:
+        lastAction = ACTION_NO_ACTION;
+        get_stimulus();
+    case ACTION_TRANSFER_STIMULUS:
+    case ACTION_TRANSFER_DATA:
+        break;
+    }
+}
+
+void MainWindow::start_sweep()
+{
+    QVector<QString> commands;
+
+    if (not ui->avgEn->isChecked()) {
+        // Single sweep when averaging is off
+        commands.push_back("AVEROFF;*OPC?");
+        commands.push_back("SING;*OPC?");
+        send_command_list(commands);
+    } else {
+        // n number of sweeps for averaging factor of n
+        commands.push_back("AVERON;*OPC?");
+        commands.push_back(QString("AVERFACT %1;*OPC?").arg(ui->avgSweeps->currentText()));
+        commands.push_back(QString("NUMG %1;*OPC?").arg(ui->avgSweeps->currentText()));
+        send_command_list(commands);
+    }
+    lastAction = ACTION_SWEEP_STARTED;
+}
+
+void MainWindow::pollHold_timeout()
+{
+    lastAction = ACTION_POLL_SWEEP_FINISH;
+    QVector<QString> commands;
+    commands.push_back("HOLD?");
+    send_command_list(commands);
+}
+
+void MainWindow::fit_trace()
+{
+    QVector<QString> commands;
+    // Auto scale both channels. Not relevant for getting the trace data but for viewing on the instrument screen
+    commands.push_back("CHAN1;*OPC?");
+    commands.push_back("AUTO;*OPC?");
+    commands.push_back("CHAN2;*OPC?");
+    commands.push_back("AUTO;*OPC?");
+    commands.push_back("CHAN1;*OPC?");
+    send_command_list(commands);
+    lastAction = ACTION_FIT_TRACE;
+}
+
+void MainWindow::get_stimulus()
+{
+    // Get stimulus frequencies
+    QVector<QString> commands;
+    commands.push_back("OUTPSTIM?");
+    send_command_list(commands);
+    lastAction = ACTION_TRANSFER_STIMULUS;
+}
+
+void MainWindow::get_trace_data()
+{
+    // Get trace data of Channel 1 in Format Real+jImag
+    QVector<QString> commands;
+    commands.push_back("OUTPFORM?");
+    send_command_list(commands);
+    lastAction = ACTION_TRANSFER_DATA;
+}
+
+void MainWindow::unpack_raw_data()
+{
+    trace_data.clear();
+
+}
+
+
+void MainWindow::on_btnStart_clicked()
+{
+    update_settings();
+    lastAction = ACTION_SETTINGS_UPDATE;
+    // Sweep will be started asynchronous after settings update was successful
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -7,7 +7,7 @@
 #include <QStateMachine>
 #include <QState>
 #include <QFinalState>
-#include <QEventTransition>
+#include <QTimer>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -24,10 +24,32 @@ public:
 private slots:
 
     void on_avgEn_stateChanged(int arg1);
-
     void on_pushButton_clicked();
+    void on_btnStart_clicked();
+
 
 private:
+
+    enum last_action_t{
+        ACTION_NO_ACTION,
+        ACTION_INSTRUMENT_REQUEST,
+        ACTION_INSTRUMENT_INIT,
+        ACTION_SETTINGS_UPDATE,
+        ACTION_SWEEP_STARTED,
+        ACTION_POLL_SWEEP_FINISH,
+        ACTION_FIT_TRACE,
+        ACTION_TRANSFER_STIMULUS,
+        ACTION_TRANSFER_DATA
+    }; //Keep track of the last GPIB action since it is asynchronous
+
+    struct trace_data_t {
+        float frequency;
+        float real;
+        float imaginary;
+    };
+
+    last_action_t lastAction;
+
     Ui::MainWindow *ui;
 
     PrologixGPIB *gpib = nullptr;
@@ -37,7 +59,20 @@ private:
     quint8 instrument_gpib_id;
     void gpib_response(QString resp);
     quint16 currentIndex;
-    QString lastCommand;
+    void update_settings();
+    void send_command_list(QVector<QString> &commands);
+    void send_command_list_finished();
+    void start_sweep();
+    QVector<QString> instrumentSettings;
+    QTimer *pollHold = nullptr; //When sweeping, poll instrument to check if sweep has finished
+    void pollHold_timeout();
+    void fit_trace();
+    void get_stimulus();
+    void get_trace_data();
+    void unpack_raw_data();
+    QString stimulus_raw;
+    QString trace_raw;
+    QVector<trace_data_t> trace_data;
 
 signals:
     void positive_response_from_instrument(); // Instrument responds with "1" when queried a *OPC? command
